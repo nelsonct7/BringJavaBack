64591d76abf3636608757ca1b4998200def21372
Nelson-wedefineapps <nelson@wedefineapps.com>
2025-11-05 09:43:31 +0530

    data structures in progress

---
+++ DataStructures/ArrayStructure.java
@@ -0,0 +1,69 @@
+
+public class ArrayStructure {
+    public static void main(String[] args) {
+        int[] nums = { 1, 8, 2, 3, 9, 4, 7, 12, 56, 85 };
+        int target = 4;
+        System.out.println("Linear search result for target : " + target);
+        int elementIndex = ArrayStructure.linearSearch(nums, target);
+        if (elementIndex == -1) {
+            System.out.println("The target element is not found in the array ");
+        } else {
+            System.out.println(
+                    "The target element " + target + " is present at the index of [" + elementIndex + "] in the array");
+        }
+        System.out.println("Binary search require array to be sorted always, so first sorting the array");
+        int[] sorted = ArrayStructure.selectionSort(nums);
+        System.out.println("Sorted array ");
+        for (int i = 0; i < sorted.length; i++) {
+            System.out.println(sorted[i]);
+        }
+        target = 66;
+        elementIndex = -1;
+        elementIndex = ArrayStructure.binarySearch(sorted, target);
+        if (elementIndex == -1) {
+            System.out.println("The element is not found in the array as per binary search");
+        } else {
+            System.out.println(
+                    "The element " + target + " is found in the array at the index of [" + elementIndex + "]");
+        }
+    }
+
+    public static int linearSearch(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == target) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public static int[] selectionSort(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i; j < nums.length; j++) {
+                if (nums[i] > nums[j]) {
+                    int temp = nums[i];
+                    nums[i] = nums[j];
+                    nums[j] = temp;
+                }
+            }
+        }
+        return nums;
+    }
+
+    public static int binarySearch(int[] nums, int target) {
+        int head = 0;
+        int tail = nums.length - 1;
+        int result = -1;
+        while (head <= tail) {
+            int mid = (head + tail) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                head = mid + 1;
+            } else {
+                tail = mid - 1;
+            }
+        }
+        return result;
+    }
+}
